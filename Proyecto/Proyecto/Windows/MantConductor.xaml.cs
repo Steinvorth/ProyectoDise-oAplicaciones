df057ad28b01e7b42d60451c5153605ce0e64bf6
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2023-11-25 13:42:00 -0600

    Detalles Condutctor funcional

--- Proyecto/Proyecto/Windows/MantConductor.xaml.cs
+++ Proyecto/Proyecto/Windows/MantConductor.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Proyecto.Base_de_Datos;
+
 
 namespace Proyecto.Windows
 {
@@ -19,9 +23,53 @@ namespace Proyecto.Windows
     /// </summary>
     public partial class MantConductor : Window
     {
+        //conexion a base de datos
+        Connect_DB db = new Connect_DB();
+        SqlConnection connection;
+
+
         public MantConductor()
         {
             InitializeComponent();
+
+            connection = db.GetConnection();
+            SetConductores();
+        }
+
+        private void SetConductores()
+        {
+            // Fetch data from the database
+            DataTable conductoresDataTable = FetchConductoresFromDatabase();
+
+            // Bind the data to the DataGrid
+            DG.ItemsSource = conductoresDataTable.DefaultView;
+        }
+
+        private DataTable FetchConductoresFromDatabase()
+        {
+            DataTable conductoresDataTable = new DataTable();
+
+            // Assume you have appropriate relationships between tables and modify the query accordingly
+            string query = "SELECT c.id, t.nombre, t.apellido, t.cedula, v.tipoVehiculo, v.placa " +
+                           "FROM ConductorVehiculo c " +
+                           "INNER JOIN Trabajadores t ON c.cedulaTrabajador = t.cedula " +
+                           "INNER JOIN Vehiculos v ON c.vehiculo_id = v.vehiculo_id";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    conductoresDataTable.Load(reader);
+                }
+
+                connection.Close();
+            }
+
+            return conductoresDataTable;
         }
     }
 }
+    
+
